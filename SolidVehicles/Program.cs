72fab475be1633e01e4105af5a48823815f57e0e
unknown <rys.mary@gmail.com>
2019-04-01 21:30:23 -0500

    unit testing practice

--- SolidVehicles/Program.cs
+++ SolidVehicles/Program.cs
@@ -1,12 +1,125 @@
 ﻿using System;
-
-namespace SolidVehicles
+using System.Linq;
+using System.Collections.Generic;
+public interface IVehicle
+{ // an int property called Wheels // an int property called Doors // an int property called PassengerCapacity // a bool property called Winged // a string property called TransmissionType // a double property called EngineVolume // a double property called MaxWaterSpeed // a double property called MaxLandSpeed // a double property called MaxAirSpeed // a method called Start() that returns void // a method called Stop() that returns void // a method called Drive() that returns void // a method called Fly() that returns void
+}
+public class JetSki : IVehicle
 {
-    class Program
+    public int Wheels { get; set; }
+    public int Doors { get; set; }
+    public int PassengerCapacity { get; set; }
+    public bool Winged { get; set; }
+    public string TransmissionType { get; set; }
+    public double EngineVolume { get; set; }
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; }
+    public double MaxAirSpeed { get; set; }
+
+    public void Drive()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("The jetski zips through the waves with the greatest of ease");
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Motorcycle : IVehicle
+{
+    public int Wheels { get; set; } = 2;
+    public int Doors { get; set; } = 0;
+    public int PassengerCapacity { get; set; }
+    public bool Winged { get; set; } = false;
+    public string TransmissionType { get; set; } = "Manual";
+    public double EngineVolume { get; set; } = 1.3;
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; } = 160.4;
+    public double MaxAirSpeed { get; set; }
+
+    public void Drive()
+    {
+        Console.WriteLine("The motorcycle screams down the highway");
+    }
+
+    public void Fly()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Cessna : IVehicle
+{
+    public int Wheels { get; set; } = 3;
+    public int Doors { get; set; } = 3;
+    public int PassengerCapacity { get; set; } = 113;
+    public bool Winged { get; set; } = true;
+    public string TransmissionType { get; set; } = "None";
+    public double EngineVolume { get; set; } = 31.1;
+    public double MaxWaterSpeed { get; set; }
+    public double MaxLandSpeed { get; set; }
+    public double MaxAirSpeed { get; set; } = 309.0;
+
+    public void Drive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Fly()
+    {
+        Console.WriteLine("The Cessna effortlessly glides through the clouds like a gleaming god of the Sun");
+    }
+
+    public void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Stop()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+
+        // Build a collection of all vehicles that fly
+
+        // With a single `foreach`, have each vehicle Fly()
+
+
+        // Build a collection of all vehicles that operate on roads
+
+        // With a single `foreach`, have each road vehicle Drive()
+
+
+
+        // Build a collection of all vehicles that operate on water
+
+        // With a single `foreach`, have each water vehicle Drive()
     }
 }
